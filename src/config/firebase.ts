5f8c8e2e759614efbf2afc80902dc93ad5577899
snyx <raphaelcr.ads@gmail.com>
2022-10-10 19:33:15 -0300

    chore: firebase has been installed and configured

---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDx5qT5oe81v_6fRKRGmVGNUa6nrrAlfWw",
+  authDomain: "cpedidos-pmp.firebaseapp.com",
+  projectId: "cpedidos-pmp",
+  storageBucket: "cpedidos-pmp.appspot.com",
+  messagingSenderId: "1083881374343",
+  appId: "1:1083881374343:web:96410eea8d5a201c636e22",
+};
+
+const app = initializeApp(firebaseConfig);
