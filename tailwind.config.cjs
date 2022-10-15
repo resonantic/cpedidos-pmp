c01846ba7ea8e7da579485c9dc6e32a8be56d267
snyx <raphaelcr.ads@gmail.com>
2022-10-15 14:41:44 -0300

    chore(ui): added color gray-700 to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     fontSize: {
       xs: 14,
@@ -8,27 +8,28 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      "2xl": 32,
+      '2xl': 32,
     },
     colors: {
-      transparent: "transparent",
-      black: "#000",
-      white: "#fff",
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
       gray: {
-        900: "#121214",
-        800: "#202024",
-        400: "#7c7c8a",
-        200: "#c4c4cc",
-        100: "#e1e1e6",
+        900: '#121214',
+        800: '#202024',
+        700: '#29292e',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
       },
       indigo: {
-        500: "#6366f1",
-        400: "#818cf8",
+        500: '#6366f1',
+        400: '#818cf8',
       },
     },
     extend: {
       fontFamily: {
-        sans: "Inter, sans-serif",
+        sans: 'Inter, sans-serif',
       },
     },
   },
