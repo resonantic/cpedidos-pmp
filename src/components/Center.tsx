9fb4f5d14be1adcc581193fce175543ce14588e2
snyx <raphaelcr.ads@gmail.com>
2022-10-15 12:59:21 -0300

    revert(components): all components has been removed

--- src/components/Center.tsx
+++
@@ -1,9 +0,0 @@
-export interface CenterProps {
-  children: React.ReactNode;
-}
-
-export function Center({ children }: CenterProps) {
-  return (
-    <div className="h-screen flex justify-center items-center">{children}</div>
-  );
-}
