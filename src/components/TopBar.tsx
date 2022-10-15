31af0c9b997d746a5c13e8fe6c1ab22806560b2f
snyx <raphaelcr.ads@gmail.com>
2022-10-15 18:22:03 -0300

    chore(components): changed appBar position to stick

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 export function TopBar() {
   return (
-    <div className="flex flex-row gap-5 items-center px-5 py-2 h-16 w-full bg-gray-800">
+    <div className="flex flex-row gap-5 items-center px-5 py-2 h-16 w-full bg-gray-800 sticky top-0">
       <h2 className="font-bold text-xl text-gray-100 whitespace-nowrap">
         Controle de Pedidos
       </h2>
