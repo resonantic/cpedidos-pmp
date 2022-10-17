40551ce5311b4fcc8e666f26336b8522dc00923f
snyx <raphaelcr.ads@gmail.com>
2022-10-17 20:58:45 -0300

    chore(ui): added a lottie animation to loading screen

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,9 +1,25 @@
-import { CircleNotch } from 'phosphor-react';
+import Lottie, { Options as LottieOptions } from 'react-lottie';
+import cartIconLoader from '../../assets/cart-icon-loader.json';
 
 export function Loading() {
+  const animationOptions: LottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: cartIconLoader,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
-    <div className="w-screen h-screen bg-neutral-900 flex flex-col items-center justify-center text-neutral-100">
-      <CircleNotch weight="bold" className="w-12 h-12 animate-spin" />
+    <div className="w-screen h-screen bg-neutral-900 flex flex-col items-center justify-center">
+      <Lottie
+        options={animationOptions}
+        height={384}
+        width={384}
+        isClickToPauseDisabled
+        style={{ cursor: 'default' }}
+      />
     </div>
   );
 }
