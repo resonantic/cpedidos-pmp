e0f0706fc02895b4a14bc7b404e29e5a55835cdd
snyx <raphaelcr.ads@gmail.com>
2022-10-19 18:53:36 -0300

    fix(ui): changed lottie package to fix warning

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,24 +1,14 @@
-import Lottie, { Options as LottieOptions } from 'react-lottie';
+import Lottie from 'lottie-react';
 import cartIconLoader from '../../assets/cart-icon-loader.json';
 
 export function Loading() {
-  const animationOptions: LottieOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: cartIconLoader,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
   return (
     <div className="w-screen h-screen bg-neutral-900 flex flex-col items-center justify-center">
       <Lottie
-        options={animationOptions}
-        height={384}
-        width={384}
-        isClickToPauseDisabled
-        style={{ cursor: 'default' }}
+        loop
+        autoplay
+        animationData={cartIconLoader}
+        className="w-96 h-96"
       />
     </div>
   );
